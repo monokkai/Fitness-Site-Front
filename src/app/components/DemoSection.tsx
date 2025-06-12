8a76178e6b9b3ffdbc9b968baad25b5922aabb2b
monokkai <egormayer08@mail.ru>
2025-06-13 00:27:50 +0200

    remade price section

--- src/app/components/DemoSection.tsx
+++ src/app/components/DemoSection.tsx
@@ -7,6 +7,8 @@ import {
   Text,
   Button,
   useColorModeValue,
+  Divider,
+  Image,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
@@ -47,15 +49,9 @@ export default function PowerfulFeatures() {
           maxW={{ base: "100%", md: "600px" }}
           rounded="2xl"
           overflow="hidden"
-          border="1px solid"
           borderColor={borderColor}
-          boxShadow={useColorModeValue("xl", "dark-lg")}
-          _hover={{
-            boxShadow: useColorModeValue("2xl", "dark-2xl"),
-            transition: "box-shadow 0.3s ease",
-          }}
         >
-          <img
+          <Image
             src="http://localhost:8081/photo/macbook.png"
             alt="MacBook with website"
             style={{ width: "100%", height: "auto", display: "block" }}
@@ -105,79 +101,126 @@ export default function PowerfulFeatures() {
         >
           Unlock Your Full Potential
         </Heading>
-        <Flex
-          direction={{ base: "column", md: "row" }}
-          justify="center"
-          align="stretch"
-          gap={8}
+        <Box
+          p={20}
+          bg={"gray.100"}
+          w={"100vw"}
+          ml={"calc(50% - 50vw)"}
+          mr={"calc(50% - 50vw)"}
         >
-          <Box
-            p={8}
-            rounded="3xl"
-            border="1px solid"
-            borderColor={borderColor}
-            boxShadow={useColorModeValue("xl", "dark-lg")}
-            flex="1"
-            maxW={{ base: "100%", md: "380px" }}
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-between"
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            justify="center"
+            align="stretch"
+            gap={8}
           >
-            <Box>
-              <Heading as="h3" size="md" mb={4} color={textColor}>
-                3 months free
-              </Heading>
-              <Text fontSize="md" mb={6} color={textColor}>
-                New subscribers get 3 months of HandFit+ free with the first
-                purchase.
-              </Text>
+            <Box
+              p={8}
+              rounded="3xl"
+              borderColor={borderColor}
+              flex="1"
+              maxW={{ base: "100%", md: "380px" }}
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+            >
+              <Box>
+                <Heading as="h3" size="md" mb={4} color={textColor}>
+                  3 months free
+                </Heading>
+                <Divider borderColor="gray.300" mb={4} />
+                <Text fontSize="md" mb={6} color={textColor}>
+                  New subscribers get 3 months of HandFit+ free with the first
+                  purchase.
+                </Text>
+              </Box>
+              <Link href="/pricing" passHref>
+                <Button
+                  size="md"
+                  colorScheme="green"
+                  bg="brand.400"
+                  borderRadius={30}
+                  _hover={{ bg: "brand.600" }}
+                >
+                  Shop now
+                </Button>
+              </Link>
             </Box>
-            <Link href="/pricing" passHref>
-              <Button
-                size="md"
-                colorScheme="green"
-                bg="brand.500"
-                borderRadius={30}
-                _hover={{ bg: "brand.600" }}
-              >
-                Shop now
-              </Button>
-            </Link>
-          </Box>
-          <Box
-            p={8}
-            rounded="3xl"
-            border="1px solid"
-            borderColor={borderColor}
-            boxShadow={useColorModeValue("xl", "dark-lg")}
-            flex="1"
-            maxW={{ base: "100%", md: "380px" }}
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-between"
-          >
-            <Box>
-              <Heading as="h3" size="md" mb={4} color={textColor}>
-                1 month free
-              </Heading>
-              <Text fontSize="md" mb={6} color={textColor}>
-                New subscribers get 1 month of HandFit+ free, then pay $9.99 per
-                month or $79.99 annually.
-              </Text>
+            <Box
+              p={8}
+              rounded="3xl"
+              borderColor={borderColor}
+              flex="1"
+              maxW={{ base: "100%", md: "380px" }}
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+            >
+              <Box>
+                <Heading as="h3" size="md" mb={4} color={textColor}>
+                  1 month free
+                </Heading>
+                <Divider borderColor="gray.300" mb={4} />
+                <Text fontSize="md" mb={6} color={textColor}>
+                  New subscribers get 1 month of HandFit+ free, then pay $9.99
+                  per month or $29.99 annually.
+                </Text>
+              </Box>
+              <Link href="/pricing" passHref>
+                <Button
+                  size="md"
+                  colorScheme="green"
+                  bg="brand.400"
+                  borderRadius={30}
+                  _hover={{ bg: "brand.600" }}
+                >
+                  Try it free
+                </Button>
+              </Link>
             </Box>
-            <Link href="/pricing" passHref>
-              <Button
-                size="md"
-                colorScheme="green"
-                bg="brand.500"
-                borderRadius={30}
-                _hover={{ bg: "brand.600" }}
-              >
-                Try it free
-              </Button>
-            </Link>
-          </Box>
-        </Flex>
+            <Box
+              p={8}
+              rounded="3xl"
+              borderColor={borderColor}
+              flex="1"
+              maxW={{ base: "100%", md: "380px" }}
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+            >
+              <Box>
+                <Heading as="h3" size="md" mb={4} color={textColor}>
+                  HandFit One
+                </Heading>
+                <Divider borderColor="gray.300" mb={4} />
+                <Text fontSize="md" mb={6} color={textColor}>
+                  Bundle HandFit+ with five other great services for one low
+                  monthly price.
+                </Text>
+              </Box>
+              <Flex direction="column" gap={2}>
+                <Link href="/pricing" passHref>
+                  <Button
+                    size="md"
+                    colorScheme="green"
+                    variant="outline"
+                    borderColor="brand.500"
+                    color="brand.500"
+                    borderRadius={30}
+                    _hover={{ bg: "brand.500", color: "white" }}
+                  >
+                    Try Apple One free
+                  </Button>
+                </Link>
+                <Link href="/pricing" passHref>
+                  <Button size="md" variant="link" color={textColor}>
+                    Learn more
+                  </Button>
+                </Link>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
       </Box>
     </Box>
   );
