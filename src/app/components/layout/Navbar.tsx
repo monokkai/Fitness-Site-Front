7e0a96776211c3dd335607c7a0d26ac49790ed50
monokkai <egormayer08@mail.ru>
2025-08-07 18:13:37 +0200

    upd + dev: updated some tasks and remade avatars on Navbar.tsx

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -203,7 +203,16 @@ const Navbar: React.FC = () => {
                   : "0 0 8px rgba(0, 0, 0, 0.2)",
               }}
             >
-              <Avatar size="sm" name={user.username} color="white" />
+              <Avatar
+                size="sm"
+                name={user.username}
+                src={
+                  user?.username
+                    ? `https://api.dicebear.com/7.x/avataaars/svg?seed=${encodeURIComponent(user.username)}`
+                    : undefined
+                }
+                bg="brand.400"
+              />
             </MenuButton>
             <MenuList
               bg="white"
@@ -213,16 +222,16 @@ const Navbar: React.FC = () => {
               boxShadow="md"
             >
               <MenuItem
-                icon={<FaDumbbell />}
-                onClick={() => (window.location.href = "/trainings")}
-              >
-                Trainings
-              </MenuItem>
-              <MenuItem
                 icon={<FaUser />}
                 onClick={() => (window.location.href = "/profile")}
               >
                 Profile
+              </MenuItem>
+              <MenuItem
+                icon={<FaDumbbell />}
+                onClick={() => (window.location.href = "/trainings")}
+              >
+                Trainings
               </MenuItem>
               <MenuDivider />
               <MenuItem
@@ -312,8 +321,15 @@ const Navbar: React.FC = () => {
                       <Avatar
                         size="sm"
                         name={user.username}
-                        bg="green.500"
+                        src={
+                          user?.username
+                            ? `https://api.dicebear.com/7.x/avataaars/svg?seed=${encodeURIComponent(user.username)}`
+                            : undefined
+                        }
+                        bg="brand.400"
                         color="white"
+                        border="2px solid"
+                        borderColor="whiteAlpha.600"
                       />
                     }
                     color={isPricingPage ? "white" : "black"}
@@ -341,16 +357,16 @@ const Navbar: React.FC = () => {
                     boxShadow="md"
                   >
                     <MenuItem
-                      icon={<FaDumbbell />}
-                      onClick={() => (window.location.href = "/trainings")}
-                    >
-                      Trainings
-                    </MenuItem>
-                    <MenuItem
                       icon={<FaUser />}
                       onClick={() => (window.location.href = "/profile")}
                     >
                       Profile
+                    </MenuItem>
+                    <MenuItem
+                      icon={<FaDumbbell />}
+                      onClick={() => (window.location.href = "/trainings")}
+                    >
+                      Trainings
                     </MenuItem>
                     <MenuDivider />
                     <MenuItem
